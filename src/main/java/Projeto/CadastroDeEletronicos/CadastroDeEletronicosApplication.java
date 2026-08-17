0153b04f2fc92ae20104621934908f31354bdf68
PedroSinhuk <pedrosinhuk123@gmail.com>
2026-08-16 23:27:21 -0300

    Feat: Cria a estrutura inicial do meu projeto, com as dependencias principais

---
+++ src/main/java/Projeto/CadastroDeEletronicos/CadastroDeEletronicosApplication.java
@@ -0,0 +1,13 @@
+package Projeto.CadastroDeEletronicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeEletronicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeEletronicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projeto/CadastroDeEletronicos/CadastroDeEletronicosApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.CadastroDeEletronicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeEletronicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
